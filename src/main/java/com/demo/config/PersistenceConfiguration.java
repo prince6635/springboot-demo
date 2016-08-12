6f3d330760118eec9e9f072787c55598246b53b7
Zi Wang <ziwang@uber.com>
2016-08-12 11:13:00 -0700

    Add multiple datasources

---
+++ src/main/java/com/demo/config/PersistenceConfiguration.java
@@ -0,0 +1,30 @@
+package com.demo.config;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistenceConfiguration {
+    // Bean: Spring bean
+    // Primay: default configuration
+    // ConfigurationProperties: match the one in application.properties
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Primary
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "datasource.flyway")
+    @FlywayDataSource
+    public DataSource flywayDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
