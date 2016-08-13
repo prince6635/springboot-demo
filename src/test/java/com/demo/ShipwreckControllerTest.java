12d31bb65f02cd2cb729a2a93500e6799cc3570e
Zi Wang <ziwang@uber.com>
2016-08-13 14:10:18 -0700

    Use mock for unit tests

---
+++ src/test/java/com/demo/ShipwreckControllerTest.java
@@ -0,0 +1,39 @@
+package com.demo;
+
+import com.demo.controller.ShipwreckController;
+import com.demo.model.Shipwreck;
+import com.demo.repository.ShipwreckRepository;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.junit.Assert.assertEquals;
+
+public class ShipwreckControllerTest {
+    @InjectMocks
+    private ShipwreckController sc;
+
+    @Mock
+    private ShipwreckRepository shipwreckRepository;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+
+    @Test
+    public void testShipwreckGet() {
+        Shipwreck sw = new Shipwreck();
+        sw.setId(1l);
+        when(shipwreckRepository.findOne(1l)).thenReturn(sw);
+
+        Shipwreck wreck = sc.get(1l);
+
+        verify(shipwreckRepository).findOne(1l);
+
+        assertEquals(1l, wreck.getId().longValue());
+    }
+}
